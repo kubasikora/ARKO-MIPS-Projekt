9f901d05b2525e040740d8d400fef4f19651d127
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2018-10-29 21:36:49 +0100

    main project init

---
+++ main.asm
@@ -0,0 +1,21 @@
+###########################################################
+#                                                         #
+#       Projekt 1 z ARKO - Architektura Komputerow        #
+#       Temat: 12. Rysowanie trojpunktowej krzywej        #
+#       Beziera na obrazku BMP.                           #
+#       Autor: Jakub Sikora                               #
+#       Prowadzący: mgr. inz. Slawomir Niespodziany       #
+#                                                         #
+###########################################################
+
+	.data
+	
+
+	.text
+	.globl main
+main: 
+
+	
+	li $v0, 10
+	syscall
+	
